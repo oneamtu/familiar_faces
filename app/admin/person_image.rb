653e1a06765869b6b2113594c9baed066511100b
Octavian Neamtu <octavian.neamtu@annkissam.com>
2015-07-25 18:49:24 -0400

    Fixed person_id.

--- app/admin/person_image.rb
+++ app/admin/person_image.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register PersonImage do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :image
+permit_params :image, :person_id
 #
 # or
 #
