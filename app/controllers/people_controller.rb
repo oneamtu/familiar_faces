bf25c4a752167c280489c0def9f96d762c987c3b
Amanda Munoz <amanda.munoz@annkissam.com>
2015-07-25 16:10:32 -0400

    Linkify person index

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,9 +1,8 @@
 class PeopleController < ApplicationController
 
   def index
-    tag_id = 1 # parms[:tag_id]
-    @tag = Tag.find(tag_id)
-    @people = TagRecord.where(tag_id: tag_id).map {|tr| tr.person}
+    @tag = Tag.find(1) #Tag.find(parms[:tag_id])
+    @people = TagRecord.where(tag_id: @tag.id).map {|tr| tr.person}
   end
 
   def show
