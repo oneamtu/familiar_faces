fa1c0c8c3ff5fef4754563fcf00fe2b9acf7480c
Isaac Selkow <isaac.selkow@annkissam.com>
2015-07-25 15:26:19 -0400

    add recreate rake task

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725183955) do
+ActiveRecord::Schema.define(version: 20150725184417) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
